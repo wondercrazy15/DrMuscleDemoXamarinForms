5874d4858e8f761c944e2ceaa38625898f4c43f2
Darshan <wondercrazy15@gmail.com>
2023-08-29 10:47:55 +0530

    Initial Commit

---
+++ DrMuscleDemoApp/DrMuscleDemoApp/Views/MainOnboardingPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DrMuscleDemoApp.Views
+{	
+	public partial class MainOnboardingPage : ContentPage
+	{	
+		public MainOnboardingPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
